0690dacd4f61f8fce2a9bb4cfd6837764a6bacbd
Alxander <onepeoplgood@gmail.com>
2018-10-23 20:25:56 +0300

    second

--- app.js
+++ app.js
@@ -24,6 +24,21 @@ UI.prototype.addBookToList = function(book) {
     `;
 
     list.appendChild(tr);
+};
+
+UI.prototype.showAlert = function(message, className) {
+    //create div
+    const div = document.createElement('div');
+    div.className =`alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+
+    container.insertBefore(div, form);
+
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    },3000)
 }
 
 //clear field
@@ -44,15 +59,22 @@ document.getElementById('book-form').addEventListener('submit', (e)=>{
 
     //instantiate book
     const book = new Book(title, author, isbn);
-    console.log(book);
-
+    
     //instantiate ui
-
     const ui = new UI();
 
-    //add book to list
-    ui.addBookToList(book);
+    //validate
+    if(title === '' || author === '' || isbn ==='') {
+        //error
+        ui.showAlert('Pleas fill in all fields', 'error');
+    } else {
+        //add book to list
+        ui.addBookToList(book);
+        //show success
+        ui.showAlert('Success append', 'success');
+        //clear fields
+        ui.clearFields();
+    }
     
-    //clear fields
-    ui.clearFields();
+    
 });
